daa8d66b852f4d23bd07d626cee0e22427681c41
@Uncle-Katt <namco2k4@gmail.com>
2024-10-21 23:09:38 +0700

    project file

---
+++ src/main/java/com/example/sd04/Sd04Application.java
@@ -0,0 +1,13 @@
+package com.example.sd04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd04Application.class, args);
+    }
+
+}
